35c9105db6e6f9e7c032f016d00d5ee12b6eb695
darren <764190362@qq.com>
2018-01-09 21:47:17 +0800

    注册实例到DI容器，在注册之前和之后都能拿到实例

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,44 +10,36 @@ namespace ConsoleApp1
             Demo();
         }
 
-        private static void Demo() 
+        private static void Demo()
         {
+            var instance = new MyInstance { Value = 88 };
             IServiceCollection services = new ServiceCollection();
-            services.AddTransient<IMyService, MyService>();
-            services.AddTransient(provider => new MyUtility(provider.GetService<IMyService>()));
+            services.AddSingleton(instance);
+
+            //在IServiceCollection中每一个service的类型是ServiceDescriptor
+            //遍历IServiceColleciton
+            //在注册到DI容器之前得到一个对象实例
+            foreach(ServiceDescriptor service in services)
+            {
+                //判断每个ServiceDescriptor类型
+                if(service.ServiceType == typeof(MyInstance))
+                {
+                    //从ServiceDescriptor中拿到正在实现的类
+                    var registeredInstance = (MyInstance)service.ImplementationInstance;
+                    Console.WriteLine("registered instance: " + registeredInstance.Value);
+                }
+            }
+
             var serviceProvider = services.BuildServiceProvider();
-            var instance = serviceProvider.GetService<MyUtility>();
-            instance.DoSth();
+            var myInstance = serviceProvider.GetService<MyInstance>();
+            Console.WriteLine("registered service by instance registration: " + myInstance.Value);
         }
 
     }
 
-    public interface IMyService
+    public class MyInstance
     {
-        void Do();
-    }
-
-    public class MyService : IMyService
-    {
-        public void Do()
-        {
-            Console.WriteLine("i am doing sth");
-        }
-    }
-
-    public class MyUtility
-    {
-        private readonly IMyService _myService;
-
-        public MyUtility(IMyService myService)
-        {
-            this._myService = myService;
-        }
-
-        public void DoSth()
-        {
-            _myService.Do();
-        }
+        public int Value { get; set; }
     }
 
 }
