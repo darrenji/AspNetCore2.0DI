b38a1c99bdccf0e2391bc8b5a6a0ada6256eaaae
darren <764190362@qq.com>
2018-01-09 20:48:53 +0800

    Microsoft.Extensions.DependencyInjection带来了DI，IServiceCollection, IServiceProvider

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddTransient<MyService>();
+            var serviceProvider = services.BuildServiceProvider();
+            var myService = serviceProvider.GetService<MyService>();
+            myService.DoSth();
+        }
+    }
+
+    public class MyService
+    {
+        public void DoSth()
+        {
+            Console.WriteLine("doing sth...");
+        }
+    }
+}
