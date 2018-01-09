a7da8f9b6a2ecefdafc571ef306ef51e9a144aa6
darren <764190362@qq.com>
2018-01-09 21:54:10 +0800

    注册泛型到DI容器

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,39 +7,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Demo();
+          
         }
 
         private static void Demo()
         {
-            var instance = new MyInstance { Value = 88 };
             IServiceCollection services = new ServiceCollection();
-            services.AddSingleton(instance);
-
-            //在IServiceCollection中每一个service的类型是ServiceDescriptor
-            //遍历IServiceColleciton
-            //在注册到DI容器之前得到一个对象实例
-            foreach(ServiceDescriptor service in services)
-            {
-                //判断每个ServiceDescriptor类型
-                if(service.ServiceType == typeof(MyInstance))
-                {
-                    //从ServiceDescriptor中拿到正在实现的类
-                    var registeredInstance = (MyInstance)service.ImplementationInstance;
-                    Console.WriteLine("registered instance: " + registeredInstance.Value);
-                }
-            }
-
+            services.AddTransient(typeof(IMyGeneric<Special>), typeof(MyGeneric<Special>));
             var serviceProvider = services.BuildServiceProvider();
-            var myInstance = serviceProvider.GetService<MyInstance>();
-            Console.WriteLine("registered service by instance registration: " + myInstance.Value);
+            var service = serviceProvider.GetService<IMyGeneric<Special>>();
         }
 
     }
 
-    public class MyInstance
+    public interface IMyGeneric<T> where T : class
     {
-        public int Value { get; set; }
+
+    }
+
+    public class MyGeneric<T> : IMyGeneric<T> where T : class
+    {
+
+    }
+
+    public class Special
+    {
+
     }
 
 }
