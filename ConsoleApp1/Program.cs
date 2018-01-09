04378835f5340bc5f916449d00a3604886c0a6e0
darren <764190362@qq.com>
2018-01-09 21:35:04 +0800

    使用工厂方式注册服务

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,102 +7,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Demo2();
+            Demo();
         }
 
-        private static void Demo()
+        private static void Demo() 
         {
             IServiceCollection services = new ServiceCollection();
-            services.AddTransient<TransientDateOperation>();
-            services.AddScoped<ScopedDateOperation>();
-            services.AddSingleton<SingletonDateOperation>();
+            services.AddTransient<IMyService, MyService>();
+            services.AddTransient(provider => new MyUtility(provider.GetService<IMyService>()));
             var serviceProvider = services.BuildServiceProvider();
-
-            Console.WriteLine();
-            Console.WriteLine("-----first request----");
-            Console.WriteLine();
-            var transientService = serviceProvider.GetService<TransientDateOperation>();
-            var scopedServce = serviceProvider.GetService<ScopedDateOperation>();
-            var singletonService = serviceProvider.GetService<SingletonDateOperation>();
-
-            Console.WriteLine();
-            Console.WriteLine("-----second request----");
-            Console.WriteLine();
-            var transientService2 = serviceProvider.GetService<TransientDateOperation>();
-            var scopedService2 = serviceProvider.GetService<ScopedDateOperation>();
-            var singletonService2 = serviceProvider.GetService<SingletonDateOperation>();
-
-            Console.WriteLine();
-            Console.WriteLine("---------");
-            Console.WriteLine();
-
+            var instance = serviceProvider.GetService<MyUtility>();
+            instance.DoSth();
         }
 
-        private static void Demo2()
+    }
+
+    public interface IMyService
+    {
+        void Do();
+    }
+
+    public class MyService : IMyService
+    {
+        public void Do()
         {
-            IServiceCollection services = new ServiceCollection();
-            services.AddTransient<TransientDateOperation>();
-            services.AddScoped<ScopedDateOperation>();
-            services.AddSingleton<SingletonDateOperation>();
-            var serviceProvider = services.BuildServiceProvider();
-
-            Console.WriteLine();
-            Console.WriteLine("-----first request----");
-            Console.WriteLine();
-            using(var scope = serviceProvider.CreateScope())
-            {
-                var transientService = scope.ServiceProvider.GetService<TransientDateOperation>();
-                var scopedService = scope.ServiceProvider.GetService<ScopedDateOperation>();
-                var singletonService = scope.ServiceProvider.GetService<SingletonDateOperation>();
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("-----second request----");
-            Console.WriteLine();
-            using (var scope = serviceProvider.CreateScope())
-            {
-                var transientService = scope.ServiceProvider.GetService<TransientDateOperation>();
-                var scopedService = scope.ServiceProvider.GetService<ScopedDateOperation>();
-                var singletonService = scope.ServiceProvider.GetService<SingletonDateOperation>();
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("---------");
-            Console.WriteLine();
+            Console.WriteLine("i am doing sth");
         }
     }
 
-    public class MyService
+    public class MyUtility
     {
+        private readonly IMyService _myService;
+
+        public MyUtility(IMyService myService)
+        {
+            this._myService = myService;
+        }
+
         public void DoSth()
         {
-            Console.WriteLine("doing sth...");
+            _myService.Do();
         }
     }
 
-    public class TransientDateOperation
-    {
-        public TransientDateOperation()
-        {
-            Console.WriteLine("Transient service is created");
-        }
-    }
-
-    public class ScopedDateOperation
-    {
-        public ScopedDateOperation()
-        {
-            Console.WriteLine("Scoped service is created");
-        }
-    }
-
-    public class SingletonDateOperation
-    {
-        public SingletonDateOperation()
-        {
-            Console.WriteLine("Singleton service is created");
-        }
-    }
-
-   
 }
