ba68b0e1c20273d15e851f1c1fcc5b4417aa5841
darren <764190362@qq.com>
2018-01-09 20:55:04 +0800

    IServiceProvider就是一个容器，Transient, Singleton,Scoped

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             IServiceCollection services = new ServiceCollection();
+
+            //Transient:service每次被请求就创建一个实例
+            //Scoped:为每个请求创建一个service实例
+            //Singleton:所有的请求用一个service实例
             services.AddTransient<MyService>();
             var serviceProvider = services.BuildServiceProvider();
+
+            //这里的IServiceProvider看做是DI容器
             var myService = serviceProvider.GetService<MyService>();
             myService.DoSth();
         }
